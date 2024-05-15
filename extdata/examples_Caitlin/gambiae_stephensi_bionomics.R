6b919489e34e2e1063ddd1819630fd0717e4d43a
caitlindmslin <cdmslin@gmail.com>
2024-05-15 11:55:12 +0200

    code for bionomics plots

---
+++ extdata/examples_Caitlin/gambiae_stephensi_bionomics.R
@@ -0,0 +1,42 @@
+## Load libraries.
+library(ggplot2)
+library(AnophelesModel)
+library(dplyr)
+library(tidyr)
+library(reshape2)
+library(stringr)
+library(ggpubr)
+library(reporter)
+
+
+# Build data frame bionomics.
+df_bionomics = vec_ent_param[, c("species_name", "M", "A0", "Chi", "endophagy")]
+df_bionomics = df_bionomics %>% filter(grepl(pattern = "Anopheles", x = species_name))
+df_bionomics = melt(df_bionomics, id.vars = c("species_name"))
+
+df_bionomics$variable = factor(df_bionomics$variable)
+df_bionomics$variable = recode_factor(df_bionomics$variable, M = "Parous Rate",
+                                      A0 = "Sac Rate",
+                                      Chi = "Human Blood\nIndex",
+                                      endophagy = "Endophagy")
+
+# Selected species bionomics.
+sf_selected_bio = df_bionomics %>% filter(species_name %in% c("Anopheles gambiae", "Anopheles stephensi"))
+sf_selected_bio$species_name = factor(sf_selected_bio$species_name,
+                                      levels = c("Anopheles farauti", "Anopheles stephensi"))
+
+# Histograms of parous rates, human blood index, sac rate, and endophagy.
+p_bio = ggplot(df_bionomics, aes(x = value)) + geom_histogram(bins = 20) +
+    geom_segment(data = sf_selected_bio, aes(x = value, y = 30, xend = value,
+                                             yend = 20, color = species_name),
+                 arrow = arrow(length = unit(0.15, "cm")), size = 1) +
+    facet_wrap(~ variable, ncol = 4) +
+    theme_bw(base_size = 16) +
+    labs( x = "Value", y = "Number of \nAnopheles Species") +
+    scale_color_discrete("Mosquito\nSpecies",
+                         labels = c(expression(italic("An. stephensi")), expression(italic("An. gambiae")))) +
+    theme(strip.background = element_rect(colour="white", fill="white")) +
+    theme(legend.text.align = 0)
+p_bio
+
+
