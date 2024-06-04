d0b7b269ad6a5a368b98df02ae527d25ca31d7a8
caitlindmslin <cdmslin@gmail.com>
2024-06-04 17:56:59 +0200

    update

--- extdata/examples_Caitlin/gambiae_stephensi_impact.R
+++ extdata/examples_Caitlin/gambiae_stephensi_impact.R
@@ -89,34 +89,28 @@ my_default_model_stephensi <- build_model_obj(vec_p = vec_p_stephensi, hosts_p =
 #### USE THE PACKAGE INTERVENTION OBJECT ####
 
 
-# Define other parameters.
-coverages <- c(seq(0, 1, by = 0.1))
-n_ip <- 100
-host_pop <- 2000
-vec_pop <- 10000
-
 # Define the intervention effects using the intervention list with examples included in the package.
 # Specify the exposure multiplier as 0.9.
 intervention_effects_vec_gambiae <- def_interventions_effects(intervention_list =  intervention_obj_examples,
                                                               model_p = my_default_model_gambiae,
-                                                              num_ip_points = n_ip, verbose = TRUE,
+                                                              num_ip_points = 100, verbose = TRUE,
                                                               specified_multiplier = 0.9)
 intervention_effects_vec_stephensi <- def_interventions_effects(intervention_list = intervention_obj_examples,
                                                                 model_p = my_default_model_stephensi,
-                                                                num_ip_points = n_ip, verbose = TRUE,
+                                                                num_ip_points = 100, verbose = TRUE,
                                                                 specified_multiplier = 0.9)
 
 # Calculate and plot the impact of interventions using the custom biting patterns.
 impacts_gambiae <- calculate_impact(interventions_vec = intervention_effects_vec_gambiae,
-                                    coverage_vec = coverages,
+                                    coverage_vec = c(seq(0, 1, by = 0.1)),
                                     model_p = my_default_model_gambiae,
-                                    Nv0 = vec_pop,
-                                    num_ip_points = n_ip)
+                                    Nv0 = 10000,
+                                    num_ip_points = 100)
 impacts_stephensi <- calculate_impact(interventions_vec = intervention_effects_vec_stephensi,
-                                      coverage_vec = coverages,
+                                      coverage_vec = c(seq(0, 1, by = 0.1)),
                                       model_p = my_default_model_stephensi,
-                                      Nv0 = vec_pop,
-                                      num_ip_points = n_ip)
+                                      Nv0 = 10000,
+                                      num_ip_points = 100)
 impacts_gambiae_plot <- plot_impact_species(impacts_gambiae, "VC_red")
 impacts_stephensi_plot <- plot_impact_species(impacts_stephensi, "VC_red")
 impacts <- ggarrange(plotlist = list(impacts_gambiae_plot, impacts_stephensi_plot), ncol = 2, nrow = 1)
@@ -136,4 +130,5 @@ impacts_gambiae_ci_plot <- plot_impact_var("Anopheles gambiae", impacts_gambiae_
 impacts_stephensi_ci_plot <- plot_impact_var("Anopheles stephensi", impacts_stephensi_ci)
 impacts_ci <- ggarrange(plotlist = list(impacts_gambiae_ci_plot, impacts_stephensi_ci_plot), ncol = 2, nrow = 1)
 impacts_ci
+
 
