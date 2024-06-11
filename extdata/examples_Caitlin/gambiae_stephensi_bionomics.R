c82a0ef0ddfbd7890bc88684a87c6527a56d22e5
caitlindmslin <cdmslin@gmail.com>
2024-06-11 23:34:36 +0200

    update

--- extdata/examples_Caitlin/gambiae_stephensi_bionomics.R
+++ extdata/examples_Caitlin/gambiae_stephensi_bionomics.R
@@ -36,19 +36,19 @@ df_bionomics$variable <- recode_factor(df_bionomics$variable, M = "Parous Rate",
 # Filter for Anopheles gambiae and Anopheles stephensi bionomics.
 sf_selected_bio <- df_bionomics %>% filter(species_name %in% c("Anopheles gambiae", "Anopheles stephensi"))
 sf_selected_bio$species_name <- factor(sf_selected_bio$species_name,
-                                       levels = c("Anopheles farauti", "Anopheles stephensi"))
+                                       levels = c("Anopheles gambiae", "Anopheles stephensi"))
+
 
 # Histograms of parous rates, human blood index, sac rate, and endophagy.
 p_bio <- ggplot(df_bionomics, aes(x = value)) + geom_histogram(bins = 20) +
     geom_segment(data = sf_selected_bio, aes(x = value, y = 30, xend = value,
                                              yend = 20, color = species_name),
-                 arrow = arrow(length = unit(0.15, "cm")), size = 1) +
+                 arrow = arrow(length = unit(0.15, "cm")), linewidth = 1) +
     facet_wrap(~ variable, ncol = 4) +
     theme_bw(base_size = 16) +
-    labs( x = "Value", y = "Number of \nAnopheles Species") +
-    scale_color_discrete("Mosquito\nSpecies",
-                         values = c("Anopheles stephensi" = "royalblue", "Anopheles gambiae" = "hotpink"),
-                         labels = c(expression(italic("An. stephensi")), expression(italic("An. gambiae")))) +
+    labs( x = "Value", y = "Number of \nAnopheles Species", color = "Anopheles Species") +
+    scale_color_manual(values = c("Anopheles stephensi" = "hotpink", "Anopheles gambiae" = "royalblue"),
+                       labels = c(expression(italic("An. gambiae")), expression(italic("An. stephensi")))) +
     theme(strip.background = element_rect(colour = "white", fill = "white")) +
     theme(legend.text.align = 0)
 p_bio
