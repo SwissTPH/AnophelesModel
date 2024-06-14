f4a3d702c2e1d2bd313a0022cd3d30acd26015b5
caitlindmslin <cdmslin@gmail.com>
2024-06-14 12:22:19 +0200

    update

--- extdata/examples_Caitlin/gambiae_stephensi_bionomics.R
+++ extdata/examples_Caitlin/gambiae_stephensi_bionomics.R
@@ -23,7 +23,6 @@ library(reporter)
 df_bionomics <- vec_ent_param[, c("species_name", "M", "A0", "Chi", "endophagy")]
 df_bionomics <- df_bionomics %>% filter(grepl(pattern = "Anopheles", x = species_name))
 df_bionomics <- melt(df_bionomics, id.vars = c("species_name"))
-
 df_bionomics$variable <- factor(df_bionomics$variable)
 df_bionomics$variable <- recode_factor(df_bionomics$variable, M = "Parous Rate",
                                        A0 = "Sac Rate", Chi = "Human Blood\nIndex",
@@ -53,4 +52,33 @@ p_bio <- ggplot(df_bionomics, aes(x = value)) + geom_histogram(bins = 20) +
     theme(legend.text.align = 0)
 p_bio
 
+
+#### MOSQUITO BIONOMICS WITH STANDARD DEVIATIONS ####
+
+
+# Create a dataframe with Anopheles gambiae and Anopheles stephensi bionomics, along with their standard deviations.
+df_bionomics_sd <- data.frame(species_name = c("Anopheles gambiae", "Anopheles gambiae", "Anopheles gambiae",
+                                               "Anopheles stephensi", "Anopheles stephensi", "Anopheles stephensi"),
+                              variable = c("Parous Rate", "Sac Rate", "Endophagy",
+                                           "Parous Rate", "Sac Rate", "Endophagy"),
+                              value = c(0.613449879, 0.641932826, 0.560413252,
+                                        0.557716117, 0.495191314, 0.399699016),
+                              min = c(0.6096308, 0.5691285, 0.5553567,
+                                      0.5254729, 0.4432327, 0.3573952),
+                              max = c(0.6172690, 0.7147371, 0.5654698,
+                                      0.5899593, 0.5471500, 0.4420028))
+
+# Plot the mosquito bionomics with their corresponding standard deviations.
+p_variance <- ggplot(df_bionomics_sd, aes(x = variable, y = value, color = species_name)) +
+    geom_point(position = position_dodge(width = 0.5), size = 3) +
+    geom_errorbar(aes(ymin = min, ymax = max), width = 0.2, position = position_dodge(width = 0.5)) +
+    theme_bw(base_size = 16) +
+    labs(x = "Bionomics Variable", y = "Value", color = "Anopheles Species") +
+    scale_color_manual(values = c("Anopheles stephensi" = "hotpink", "Anopheles gambiae" = "royalblue"),
+                       labels = c(expression(italic("An. gambiae")), expression(italic("An. stephensi")))) +
+    theme(strip.background = element_rect(colour = "white", fill = "white")) +
+    theme(legend.text.align = 0) +
+    facet_wrap(~ variable, scales = "free_y") +
+    theme(axis.text.x = element_blank())
+p_variance
 
