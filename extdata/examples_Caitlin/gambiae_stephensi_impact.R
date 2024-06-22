12d2f17d236c1c9d2e61d0a3393efad863ae084f
caitlindmslin <cdmslin@gmail.com>
2024-06-22 22:25:34 +0200

    sensitivity analysis

--- extdata/examples_Caitlin/gambiae_stephensi_impact.R
+++ extdata/examples_Caitlin/gambiae_stephensi_impact.R
@@ -101,6 +101,15 @@ intervention_effects_stephensi <- def_interventions_effects(intervention_list = 
                                                             num_ip_points = 100, verbose = TRUE,
                                                             specified_multiplier = NULL)
 
+intervention_effects_gambiae <- def_interventions_effects(intervention_list = intervention_obj_examples,
+                                                          model_p = my_default_model_gambiae,
+                                                          num_ip_points = 100, verbose = TRUE,
+                                                          specified_multiplier = NULL)
+intervention_effects_stephensi <- def_interventions_effects(intervention_list = intervention_obj_examples,
+                                                            model_p = my_default_model_stephensi,
+                                                            num_ip_points = 100, verbose = TRUE,
+                                                            specified_multiplier = NULL)
+
 # Calculate and plot the impact of interventions using the custom biting patterns.
 impacts_gambiae <- calculate_impact(interventions_vec = intervention_effects_gambiae,
                                     coverage_vec = c(seq(0, 1, by = 0.1)),
